86fe9ed0e6f5ae2195cb036872837f487b4c4451
MrSedan <elpashevs@bk.ru>
2020-02-08 23:27:06 +0300

    Initial

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/MrSedan/go-rest-api/internal/app/apiserver"
+	"github.com/pelletier/go-toml"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	data, err := toml.LoadFile(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data.Unmarshal(config)
+
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+}
