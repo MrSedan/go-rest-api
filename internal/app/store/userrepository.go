8f0d617e664d506f06ef26f87186d1f4252cf02a
MrSedan <elpashevs@bk.ru>
2020-02-09 21:03:17 +0300

    Error fixed

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -21,15 +21,15 @@ func (r *UserRepository) Create(u *model.User) (*model.User, error) {
 
 // FindByEmail ...
 func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
-	u:= &model.User{}
-	if err:=r.store.db.QueryRow(
+	u := &model.User{}
+	if err := r.store.db.QueryRow(
 		"SELECT id, email, encrypted_password FROM users WHERE email = $1",
 		email,
 	).Scan(
-		&u.ID, 
-		&u.Email, 
-		&u.EncryptedPassword
-	); err!=nil{
+		&u.ID,
+		&u.Email,
+		&u.EncryptedPassword,
+	); err != nil {
 		return nil, err
 	}
 	return u, nil
